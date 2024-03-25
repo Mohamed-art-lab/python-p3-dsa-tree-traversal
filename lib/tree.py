453c584453d9af90063c7f7952420b53e0ef4586
Mohamed-art-lab <mohamed.farah@student.moringaschool.com>
2024-03-25 13:50:11 +0300

    completed assignment

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        if self.root is None:
+            return None
+
+        nodes_to_visit = [self.root]
+
+        while nodes_to_visit:
+            node = nodes_to_visit.pop(0)
+            if node['id'] == id:
+                return node
+            nodes_to_visit.extend(node.get('children', []))
+
+        return None
